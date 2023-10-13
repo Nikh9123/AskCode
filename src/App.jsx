25d957f7bf38ebd3ff470b0cafc79a369752928a
nikh9123 <satyamnikhil1523@gmail.com>
2023-10-13 23:30:16 +0530

    dark and light theme of the application is added | logo and images also created but i need to change it in my own application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Button, Container } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 
 function App() {
 	return (
 		<Container maxW="620px">
+			<Header />
 			<Routes>
 				<Route path="/:username" element={<UserPage />} />
 				<Route path="/:username/post/:pid" element={<PostPage />} />
