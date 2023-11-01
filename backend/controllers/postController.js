1f447d7ffb2432e19971b9281dc105455cb3b1db
nikh9123 <satyamnikhil1523@gmail.com>
2023-11-01 22:27:50 +0530

    reply to posts and user feed function implemented

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -172,5 +172,102 @@ const likeUnlikePost = async (req, res) => {
 	}
 };
 
+const replyToPost = async(req,res)=>{
 
-export { getAllPosts, getPost, createPost, deletePost, likeUnlikePost };
+  try {
+  const {text} = req.body ;
+  const {postId} = req.params ;
+  const userId = req.user._id.toString() ;
+  const profilePic = req.user.profilePic ;
+  const username = req.user.username ;
+
+  if(!text)
+  {
+    return res.status(400).json({
+      status:"fail",
+      message:"please enter text to reply"
+    })
+  }
+  const post = await Post.findById(postId) ;
+
+  if(!post)
+  {
+    res.status(404).json({
+      status:"fail",
+      message:"post not found"
+    })
+  }
+  
+  const newReply = {
+    userId,
+    text,
+    profilePic,
+    username
+  }
+  post.replies.push(newReply) ;
+  await post.save() ;
+
+  res.status(201).json({
+    status:"success",
+    message:"reply added successfully",
+    data: post.replies
+  });
+
+  } catch (err) {
+    console.error("Error from replyToPost: ", err.message);
+    res.status(500).json({
+      status:"fail",
+      message:"unable to reply to post. Try again later"
+    })
+  }
+}
+
+
+const getUserFeedPost = async(req,res) => {
+  try {
+    const user = await User.findById(req.user._id) ;
+    
+    if(!user)
+    {
+      return res.status(404).json({
+        status:"fail",
+        message:"user not found"
+      })
+    }
+
+    //get all the users that the current user is following
+    const following = user.following ;
+    
+    //get all the posts of the users that the current user is following
+    const FollowingPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt:-1}) ;
+
+    //get all the users who follows the current user
+    const followers = user.followers ;
+
+    //get all the posts of the users who follows the current user
+    const FollowersPosts = await Post.find({postedBy:{$in:followers}}).sort({createdAt:-1}) ;
+
+    if(!FollowingPosts && !FollowersPosts)
+    {
+      return res.status(404).json({
+        status:"fail",
+        message:"no posts found"
+      })
+    }
+    
+    res.status(200).json({
+      status:"success",
+      message:"your feed",
+      // data: FollowingPosts.concat(FollowersPosts)//this will return an array of concatenated posts of the users that the current user is following and the users who follows the current user
+      data: [...FollowingPosts,...FollowersPosts]
+    })
+
+  } catch (error) {
+    console.error("Error from getUserFeedPost : ", err.message);
+    res.status(500).json({
+      status:"fail",
+      message:"unable to get your feed. Try again later"
+    })
+  }
+}
+export { getAllPosts, getPost, createPost, deletePost, likeUnlikePost , replyToPost , getUserFeedPost};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { createPost, deletePost, getAllPosts, getPost , likeUnlikePost} from "../controllers/postController.js";
+import { createPost, deletePost, getAllPosts, getPost , getUserFeedPost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -8,9 +8,11 @@ router.get("/", getAllPosts).get("/:postId", getPost);
 
 
 router.use(protectRoute);
+router.post("/feed", getUserFeedPost);
 router.post("/create", createPost);
 router.post("/delete/:postId", deletePost)
 router.post("/like/:postId", likeUnlikePost)
+router.post("/reply/:postId", replyToPost)
 
 
 export default router;
